bca084c2824536d96ae20a1f6a987c359a386504
Yoshikawa Takamichi <t-yoshikawa@un-t.com>
2020-05-15 18:34:06 +0900

    追加

--- src/Data/PrecipitationData.php
+++ src/Data/PrecipitationData.php
@@ -3,7 +3,7 @@ namespace App\Data;
 
 use App\Data\Subject;
 use App\Display\Observer;
-
+use SplFileObject;
 
 /**
  * Description of PrecipitationData
@@ -15,9 +15,11 @@ class PrecipitationData extends Subject {
 
     public function getLatestData($url) {
         $data = file_get_contents($url);
-
+        // SJISをUTF8に変換
         $data = mb_convert_encoding($data, 'UTF-8', 'SJIS-win');
-        
+
+
+
 
     }
 
--- src/File/GetCsvFile.php
+++ src/File/GetCsvFile.php
@@ -1,11 +1,36 @@
 <?php
-namespace App\File;
-use SplFileObject;
 
+namespace App\File;
+
+use SplFileObject;
 
 /**
  * Description of GetCsvFile
  */
-class GetCsvFile {
-    //put your code here
+class CsvFile {
+
+    public $fileObject;
+    public $data;
+
+    public function __construct($path) {
+        $data = file_get_contents($path);
+        //文字コードの変換（UTF-8 → SJIS-win）
+        $data = mb_convert_encoding($data, 'UTF-8', 'SJIS-win');
+        // 一時ファイルの作成
+        $temp = tmpfile();
+        // メタデータからファイルパスを取得して読み込み
+        $meta = stream_get_meta_data($temp);
+        // 一時ファイル書き込み
+        fwrite($temp, $data);
+        // ファイルポインタの位置を先頭に
+        rewind($temp);
+        //--------------------------------------------------
+        // ファイルの読み込み
+        //--------------------------------------------------
+        // CSVファイルの読み込み
+        $this->fileObject = new SplFileObject($meta['uri'], 'rb');
+        $this->fileObject->setFlags(SplFileObject::READ_CSV);
+        $this->fileObject->setCsvControl("\t", "\"");
+    }
+
 }
