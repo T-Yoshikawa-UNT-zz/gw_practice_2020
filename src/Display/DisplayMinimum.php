2cadb45dee2be8e32a3ece2235ab43d7244ff54d
Yoshikawa Takamichi <t-yoshikawa@un-t.com>
2020-05-18 17:41:30 +0900

    修正

--- src/Display/DisplayMinimum.php
+++ src/Display/DisplayMinimum.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Display;
 
 use App\Display\Observer;
@@ -7,22 +8,19 @@ use App\Data\PrecipitationData;
 /**
  * Description of AllDisplay
  */
-class DisplayMinimum implements Observer{
-    
-    public function __construct() {
-        ;
-    }
+class DisplayMinimum implements Observer {
 
-        public function update($subject) {
+    public function update($subject) {
         foreach ($subject->data as $record) {
-            
+
             if (!empty($record[0])) {
                 printf('----------------------------------------------' . "\n");
-                printf('観測地点：' . $record[1]. ' ' . $record[2] . "\n");
-                printf('観測時間：' . $record[4]. '/' . $record[5] . '/' . $record[6] . ' ' . $record[7] . ':' . $record[8] . "\n");
-                printf('降水量：' . $record[9]. ' mm' . "\n");
+                printf('観測地点：' . $record[1] . ' ' . $record[2] . "\n");
+                printf('観測時間：' . $record[4] . '/' . $record[5] . '/' . $record[6] . ' ' . $record[7] . ':' . $record[8] . "\n");
+                printf('降水量：' . $record[9] . ' mm' . "\n");
                 printf('----------------------------------------------' . "\n");
             }
         }
     }
+
 }
